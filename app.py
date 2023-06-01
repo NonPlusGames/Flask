06130b5fd697435456672d691153c215dfac3da5
Rafael Mota <rmota@nonplusgames.com>
2023-05-31 23:31:46 -0400

    final

--- app.py
+++ app.py
@@ -4,7 +4,8 @@
 from flask import Flask
 # from flask import render_template
 # from flask import request
-
+# from flask import redirect
+# from model import shout
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -55,9 +56,52 @@ def secret():
 #     return render_template()
 
 # -- Form --
-# @app.route('/sendBreakfast', methods=['GET', 'POST'])
+# @app.route('/sendBreakfast')
 # def handleBreakfast():
 #     return "Breakfast should appear here"
+
+# -- Form with GET and POST --
+# @app.route('/sendBreakfast', methods=['GET', 'POST'])
+# def handleBreakfast():
+#     if request.method == 'GET':
+#         return "You're getting the breakfast page!"
+#     else:
+#         return "You're posting to the breakfast page"
+
+# -- Model --
+# @app.route('/form')
+# def handleForm():
+    # nickname = request.form['nickname']
+    # breakfast = request.form['breakfast']
+    # loud_name = shout(nickname)
+    # return "Hello, " + loud_name + "! I hear you had " + breakfast + " for breakfast! Sounds delicious."
+
+# -- ADVANCED TOPICS --
+
+# -- Redirect --
+# @app.route('/sendBreakfast', methods=['GET', 'POST'])
+# def handleBreakfast():
+#     if request.method == 'GET':
+#         return redirect('/notFound')
+#     else:
+#         ... #code to handle `POST` request
+
+# @app.route('/notFound')
+# def handle404():
+#     return render_template('404.html')
+#     # Note that this assumes you've created a 404.html template
+
+# -- Logic in Templates --
+# @app.route('/logic')
+# def logic():
+#     name="Fred"
+#     return render_template('logic.html', name=name)
+
+# -- Block Content --
+# @app.route('/blockcontent')
+# def blockcontent():
+#     name="Fred"
+#     return render_template('blockcontent.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
